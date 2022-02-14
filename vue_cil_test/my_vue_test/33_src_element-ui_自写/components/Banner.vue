ea54e8840721bc99c86c2455228ab5a7b34d8e3d
kai.kang <kangkai@1314>
2022-02-14 09:36:59 +0800

    element-ui

---
+++ vue_cil_test/my_vue_test/33_src_element-ui_自写/components/Banner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lujing">
+    <button @click="houtui">后退</button>
+    <button @click="qianjin">前进</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:'Banner',
+  methods:{
+    houtui(){
+      this.$router.back()
+    },
+    qianjin(){
+      this.$router.forward()
+    }
+  }
+}
+</script>
+
+<style>
+  .lujing{
+    margin-bottom: 10px;
+  }
+</style>
---
+++ vue_cil_test/my_vue_test/33_src_element-ui_自写/components/Message.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="m in messagelist" :key="m.id">
+				<router-link :to="{
+				  path:'/home/message/detail',
+				  query:{id:m.id,title:m.title}
+				}">
+          message{{m.id}}
+        </router-link>
+        <button @click="pushShow(m)">pushShow</button>
+			</li>
+		</ul>
+    <router-view></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Message',
+    data(){
+		  return{
+		    messagelist:[
+          {id:'001',title:'消息001'},
+          {id:'002',title: '消息002'},
+          {id:'003',title: '消息003'}
+        ]
+      }
+    },
+    methods:{
+      pushShow(m){
+        this.$router.push({
+          path:'/home/message/detail',
+          query:{id:m.id,title:m.title}
+        })
+      }
+
+      }
+
+	}
+</script>
